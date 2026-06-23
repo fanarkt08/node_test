74730b510954f7dc75e06f5c8395ee93543626a8
fanarkt08 <fanarkt08@gmail.com>
2026-06-23 15:39:02 +0200

    activity 12

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,5 +1,15 @@
-export const signup = (req, res) => {
-    res.send('You are signup');
+import { prisma } from '../../app.js';
+
+export const signup = async (req, res) => {
+    const { firstName, lastName, email, password } = req.body;
+    try {
+        const user = await prisma.user.create({
+            data: { firstName, lastName, email, password }
+        });
+        res.send(user);
+    } catch (error) {
+        res.send(error);
+    }
 };
 
 export const login = (req, res) => {
