8fb16d0e239431e6bfb8a2b885a10e0cfc86c446
fanarkt08 <fanarkt08@gmail.com>
2026-06-24 09:54:49 +0200

    activity 14

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,12 @@
-export const readAll = (req, res) => {
-    res.send('List of woods');
+import { prisma } from '../../app.js';
+
+export const readAll = async (req, res) => {
+    try {
+        const woods = await prisma.wood.findMany();
+        res.json(woods);
+    } catch (error) {
+        res.status(error.status || 500).send({
+            message: error.message || 'Internal server error'
+        });
+    }
 };
